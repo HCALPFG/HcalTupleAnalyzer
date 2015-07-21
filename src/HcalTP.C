c6c22b7d461603bc89800b6ed0d0f1cf15e0706c
lucien1011 <lucien1011@gmail.com>
2015-07-21 15:58:29 +0200

    added linking between HBHE and TP

--- src/HcalTP.C
+++ src/HcalTP.C
@@ -7,10 +7,9 @@ HcalTP::HcalTP(Collection& c, unsigned short i, short j ):
   Object(c,i){}
 
 
-float  HcalTP::Et()                   {return m_collection -> GetData() -> HcalTriggerPrimitiveCompressedEtSOI  -> at(m_raw_index);};
-int    HcalTP::ieta()                 {return m_collection -> GetData() -> HcalTriggerPrimitiveIEta     -> at(m_raw_index);};
-int    HcalTP::iphi()                 {return m_collection -> GetData() -> HcalTriggerPrimitiveIPhi             -> at(m_raw_index);};
-int    HcalTP::fineGrain()            {return m_collection -> GetData() -> HcalTriggerPrimitiveFineGrainSOI             -> at(m_raw_index);};
-int    HcalTP::presamples()           {return m_collection -> GetData() -> HcalTriggerPrimitivePresamples       -> at(m_raw_index);};
-
-
+float HcalTP::Et() {return m_collection -> GetData() -> HcalTriggerPrimitiveCompressedEtSOI  -> at(m_raw_index);};
+int HcalTP::ieta() {return m_collection -> GetData() -> HcalTriggerPrimitiveIEta     -> at(m_raw_index);};
+int HcalTP::iphi() {return m_collection -> GetData() -> HcalTriggerPrimitiveIPhi             -> at(m_raw_index);};
+int HcalTP::fineGrain() {return m_collection -> GetData() -> HcalTriggerPrimitiveFineGrainSOI             -> at(m_raw_index);};
+int HcalTP::presamples() {return m_collection -> GetData() -> HcalTriggerPrimitivePresamples       -> at(m_raw_index);};
+std::vector < int > HcalTP::HBHEIndices() {return m_collection -> GetData() -> HcalTriggerPrimitiveHBHEDigiIndex -> at(m_raw_index);};
