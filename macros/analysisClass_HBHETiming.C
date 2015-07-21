8e6b08432719ca04e2852d9ce7486ba38ee4d167
lucien1011 <lucien1011@gmail.com>
2015-07-21 16:10:16 +0200

    HBHE Timing

---
+++ macros/analysisClass_HBHETiming.C
@@ -0,0 +1,59 @@
+#include "analysisClass.h"
+#include "HcalTupleTree.h"
+#include "HBHEDigi.h"
+
+void analysisClass::loop(){
+  
+  //--------------------------------------------------------------------------------
+  // Declare HCAL tree(s)
+  //--------------------------------------------------------------------------------
+
+  HcalTupleTree * tuple_tree = getTree<HcalTupleTree>("tuple_tree");
+  int n_events = tuple_tree -> fChain -> GetEntries();
+  std::cout << "n events = " << n_events << std::endl;
+  
+  //--------------------------------------------------------------------------------
+  // Turn on/off branches
+  //--------------------------------------------------------------------------------
+  
+  tuple_tree -> fChain -> SetBranchStatus("*"               , kFALSE);
+  tuple_tree -> fChain -> SetBranchStatus("run"             , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("event"           , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("ls"              , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiFC"        , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiDepth"     , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiIEta"      , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiIPhi"      , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiSize"      , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiRecEnergy" , kTRUE);
+  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiRecTime"   , kTRUE);
+
+  
+  //--------------------------------------------------------------------------------
+  // Loop
+  //--------------------------------------------------------------------------------
+  int nHBHEDigis;
+
+  TH1F * recHitTiming = makeTH1F("RecTiming",400,-200,200.);
+  TH1F * recHitEnergy = makeTH1F("Energy",100,0,10.);
+
+  for (int i = 0; i < n_events; ++i){
+    
+    tuple_tree -> GetEntry(i);
+    if ( (i + 1) % 1000 == 0 ) std::cout << "Processing event " << i + 1 << "/" << n_events << std::endl;
+
+    CollectionPtr hbheDigis (new Collection(*tuple_tree, tuple_tree -> HBHEDigiIEta -> size()));
+    
+    nHBHEDigis = hbheDigis -> GetSize();
+
+    for (int iHBHEDigi = 0; iHBHEDigi < nHBHEDigis; ++iHBHEDigi){
+      HBHEDigi hbheDigi = hbheDigis -> GetConstituent<HBHEDigi>(iHBHEDigi);
+
+      if (hbheDigi.energy() < 5) continue;
+      recHitTiming -> Fill(hbheDigi.recHitTime());
+      recHitEnergy -> Fill(hbheDigi.energy());
+
+
+    }; 
+  };
+}
