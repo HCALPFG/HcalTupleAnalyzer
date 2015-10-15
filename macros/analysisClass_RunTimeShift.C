54f432c71edf7049d20ef21e852c14a86c94209e
lucien1011 <lucien1011@gmail.com>
2015-10-16 01:12:54 +0200

    added separate plot

--- macros/analysisClass_RunTimeShift.C
+++ macros/analysisClass_RunTimeShift.C
@@ -55,6 +55,13 @@ void analysisClass::loop(){
   //--------------------------------------------------------------------------------
   std::map<int,std::map<int,TH1F*>> RecHitTiming;
   std::map<int,std::map<int,TH1F*>> AvgTiming;
+
+  // std::map<int,std::map<int,TH1F*>> HBRecHitTiming;
+  // std::map<int,std::map<int,TH1F*>> HBAvgTiming;
+
+  // std::map<int,std::map<int,TH1F*>> HERecHitTiming;
+  // std::map<int,std::map<int,TH1F*>> HEAvgTiming;
+
   int hash;
   char histName[100];
   
@@ -80,7 +87,18 @@ void analysisClass::loop(){
     if (AvgTiming.find(runNumber) == AvgTiming.end()){
       AvgTiming[runNumber] = std::map<int,TH1F*>();
     };
-
+    // if (HBRecHitTiming.find(runNumber) == HBRecHitTiming.end()){
+    //   HBRecHitTiming[runNumber] = std::map<int,TH1F*>();
+    // };
+    // if (HBAvgTiming.find(runNumber) == HBAvgTiming.end()){
+    //   HBAvgTiming[runNumber] = std::map<int,TH1F*>();
+    // };
+    // if (HERecHitTiming.find(runNumber) == HERecHitTiming.end()){
+    //   HBRecHitTiming[runNumber] = std::map<int,TH1F*>();
+    // };
+    // if (HEAvgTiming.find(runNumber) == HEAvgTiming.end()){
+    //   HEAvgTiming[runNumber] = std::map<int,TH1F*>();
+    // };
 
     int nHBHEDigis = hbheDigis -> GetSize();
     for (int iHBHEDigi = 0; iHBHEDigi < nHBHEDigis; ++iHBHEDigi){
@@ -89,6 +107,7 @@ void analysisClass::loop(){
       if ( hbheDigi.energy() < 5.0 ) continue;
 
       hash = getHash( hbheDigi.ieta() , hbheDigi.iphi() , hbheDigi.depth() );
+      
       if ( RecHitTiming[runNumber].find(hash) == RecHitTiming[runNumber].end() ){
         sprintf(histName, "%d_RecHitTiming_HBHE_%d_%d_%d", runNumber , hbheDigi.ieta(), hbheDigi.iphi(), hbheDigi.depth());
         RecHitTiming[runNumber].insert(std::pair<int,TH1F*>(hash, makeTH1F(histName,100,-10.,10.)));
@@ -102,8 +121,123 @@ void analysisClass::loop(){
       RecHitTiming[runNumber][hash] -> Fill(hbheDigi.recHitTime());
       AvgTiming[runNumber][hash] -> Fill(hbheDigi.time());
 
+      // if (hbheDigi.ieta() <= 16){
+      //   if ( HBRecHitTiming[runNumber].find(hash) == HBRecHitTiming[runNumber].end() ){
+      //     sprintf(histName, "%d_RecHitTiming_HB_%d_%d_%d", runNumber , hbheDigi.ieta(), hbheDigi.iphi(), hbheDigi.depth());
+      //     HBRecHitTiming[runNumber].insert(std::pair<int,TH1F*>(hash, makeTH1F(histName,100,-10.,10.)));
+      //   };
+
+      //   if ( HBAvgTiming[runNumber].find(hash) == HBAvgTiming[runNumber].end() ){
+      //     sprintf(histName, "%d_HBAvgTiming_HBHE_%d_%d_%d", runNumber , hbheDigi.ieta(), hbheDigi.iphi(), hbheDigi.depth());
+      //     HBAvgTiming[runNumber].insert(std::pair<int,TH1F*>(hash, makeTH1F(histName,100,0.,10.)));
+      //   };
+
+      //   HBRecHitTiming[runNumber][hash] -> Fill(hbheDigi.recHitTime());
+      //   HBAvgTiming[runNumber][hash] -> Fill(hbheDigi.time());
+      // };
+
+      // if ((hbheDigi.ieta() > 16) and (hbheDigi.ieta() <= 29)){
+      //   if ( HERecHitTiming[runNumber].find(hash) == HERecHitTiming[runNumber].end() ){
+      //     sprintf(histName, "%d_RecHitTiming_HE_%d_%d_%d", runNumber , hbheDigi.ieta(), hbheDigi.iphi(), hbheDigi.depth());
+      //     HERecHitTiming[runNumber].insert(std::pair<int,TH1F*>(hash, makeTH1F(histName,100,-10.,10.)));
+      //   };
+
+      //   if ( HEAvgTiming[runNumber].find(hash) == HEAvgTiming[runNumber].end() ){
+      //     sprintf(histName, "%d_AvgTiming_HE_%d_%d_%d", runNumber , hbheDigi.ieta(), hbheDigi.iphi(), hbheDigi.depth());
+      //     HEAvgTiming[runNumber].insert(std::pair<int,TH1F*>(hash, makeTH1F(histName,100,0.,10.)));
+      //   };
+
+      //   HERecHitTiming[runNumber][hash] -> Fill(hbheDigi.recHitTime());
+      //   HEAvgTiming[runNumber][hash] -> Fill(hbheDigi.time());
+      // };
+
     };
   };
+  
+  // TH1F * graph = makeTH1F("HBRecHitTiming_vs_RunNumber",HBRecHitTiming.size(),-0.5,HBRecHitTiming.size()-0.5);
+  // char binLabel[100];
+  // int iBin = 1;
+  // for (std::map<int,std::map<int,TH1F*>>::iterator it = HBRecHitTiming.begin(); it != HBRecHitTiming.end(); it++){
+  //   double sum = 0;
+  //   double error2 = 0;
+  //   int count = 0;
+  //   int channelCount = 0;
+  //   for (std::map<int,TH1F*>::iterator it2 = it -> second.begin(); it2 != it -> second.end(); it2++){
+  //     channelCount++;
+  //     count += it2 -> second -> Integral(0,101);
+  //     sum += (it2 -> second -> Integral(0,101))*(it2 -> second -> GetMean());
+  //     error2 += (it2 -> second -> GetRMS())*(it2 -> second -> GetRMS())*( it2 -> second -> Integral(0,101));
+  //   };
+
+  //   graph -> SetBinContent( iBin , sum/count  );
+  //   graph -> SetBinError(iBin , sqrt(error2/count/channelCount) );
+  //   sprintf(binLabel,"%d",it -> first);
+  //   graph -> GetXaxis() -> SetBinLabel( iBin , binLabel );
+  //   iBin++;
+  // };
+  // 
+  // TH1F * graph2 = makeTH1F("HBAvgTiming_vs_RunNumber",HBAvgTiming.size(),-0.5,HBAvgTiming.size()-0.5);
+  // iBin = 1;
+  // for (std::map<int,std::map<int,TH1F*>>::iterator it = HBAvgTiming.begin(); it != HBAvgTiming.end(); it++){
+  //   double sum = 0;
+  //   double error2 = 0;
+  //   int count = 0;
+  //   int channelCount = 0;
+  //   for (std::map<int,TH1F*>::iterator it2 = it -> second.begin(); it2 != it -> second.end(); it2++){
+  //     channelCount++;
+  //     count += it2 -> second -> Integral(0,101);
+  //     sum += (it2 -> second -> Integral(0,101))*(it2 -> second -> GetMean());
+  //     error2 += (it2 -> second -> GetRMS())*(it2 -> second -> GetRMS())*( it2 -> second -> Integral(0,101));
+  //   };
+
+  //   graph2 -> SetBinContent( iBin , sum/count  );
+  //   graph2 -> SetBinError(iBin , sqrt(error2/count/channelCount) );
+  //   sprintf(binLabel,"%d",it -> first);
+  //   graph2 -> GetXaxis() -> SetBinLabel( iBin , binLabel );
+  //   iBin++;
+  // };
+
+  // TH1F * graph3 = makeTH1F("HERecHitTiming_vs_RunNumber",HERecHitTiming.size(),-0.5,HERecHitTiming.size()-0.5);
+  // iBin = 1;
+  // for (std::map<int,std::map<int,TH1F*>>::iterator it = HERecHitTiming.begin(); it != HERecHitTiming.end(); it++){
+  //   double sum = 0;
+  //   double error2 = 0;
+  //   int count = 0;
+  //   int channelCount = 0;
+  //   for (std::map<int,TH1F*>::iterator it2 = it -> second.begin(); it2 != it -> second.end(); it2++){
+  //     channelCount++;
+  //     count += it2 -> second -> Integral(0,101);
+  //     sum += (it2 -> second -> Integral(0,101))*(it2 -> second -> GetMean());
+  //     error2 += (it2 -> second -> GetRMS())*(it2 -> second -> GetRMS())*( it2 -> second -> Integral(0,101));
+  //   };
+
+  //   graph3 -> SetBinContent( iBin , sum/count  );
+  //   graph3 -> SetBinError(iBin , sqrt(error2/count/channelCount) );
+  //   sprintf(binLabel,"%d",it -> first);
+  //   graph3 -> GetXaxis() -> SetBinLabel( iBin , binLabel );
+  //   iBin++;
+  // };
+  // 
+  // TH1F * graph4 = makeTH1F("HEAvgTiming_vs_RunNumber",HEAvgTiming.size(),-0.5,HEAvgTiming.size()-0.5);
+  // iBin = 1;
+  // for (std::map<int,std::map<int,TH1F*>>::iterator it = HEAvgTiming.begin(); it != HEAvgTiming.end(); it++){
+  //   double sum = 0;
+  //   double error2 = 0;
+  //   int count = 0;
+  //   int channelCount = 0;
+  //   for (std::map<int,TH1F*>::iterator it2 = it -> second.begin(); it2 != it -> second.end(); it2++){
+  //     channelCount++;
+  //     count += it2 -> second -> Integral(0,101);
+  //     sum += (it2 -> second -> Integral(0,101))*(it2 -> second -> GetMean());
+  //     error2 += (it2 -> second -> GetRMS())*(it2 -> second -> GetRMS())*( it2 -> second -> Integral(0,101));
+  //   };
+
+  //   graph4 -> SetBinContent( iBin , sum/count  );
+  //   graph4 -> SetBinError(iBin , sqrt(error2/count/channelCount) );
+  //   sprintf(binLabel,"%d",it -> first);
+  //   graph4 -> GetXaxis() -> SetBinLabel( iBin , binLabel );
+  //   iBin++;
+  // };
 
   TH1F * graph = makeTH1F("RecHitTiming_vs_RunNumber",RecHitTiming.size(),-0.5,RecHitTiming.size()-0.5);
   char binLabel[100];
