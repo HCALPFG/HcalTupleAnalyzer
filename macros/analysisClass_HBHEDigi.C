8cd60e711d2b2e650ba99970d632127b52dc3660
lucien1011 <lucien1011@gmail.com>
2015-11-05 00:56:57 +0100

    general macros for individual digi

--- macros/analysisClass_HBHEDigi.C
+++ macros/analysisClass_HBHEDigi.C
@@ -13,7 +13,7 @@ void analysisClass::loop(){
   // Declare HCAL tree(s)
   //--------------------------------------------------------------------------------
 
-  HcalTupleTree * tuple_tree = getTree<HcalTupleTree>("tuple_tree");
+  HcalTupleTree * tuple_tree = getTree<HcalTupleTree>("skim_tree");
   int n_events = tuple_tree -> fChain -> GetEntries();
   std::cout << "n events = " << n_events << std::endl;
   
@@ -26,12 +26,10 @@ void analysisClass::loop(){
   tuple_tree -> fChain -> SetBranchStatus("event"           , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("ls"              , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("HBHEDigiFC"        , kTRUE);
-  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiDepth"     , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("HBHEDigiIEta"      , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("HBHEDigiIPhi"      , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("HBHEDigiSize"      , kTRUE);
   tuple_tree -> fChain -> SetBranchStatus("HBHEDigiRecEnergy" , kTRUE);
-  tuple_tree -> fChain -> SetBranchStatus("HBHEDigiRecTime"   , kTRUE);
   
   //--------------------------------------------------------------------------------
   // Loop
@@ -40,7 +38,9 @@ void analysisClass::loop(){
 
   TH1F * recHitTiming = makeTH1F("RecTiming",400,-200,200.);
   TH1F * recHitEnergy = makeTH1F("Energy",100,0,10.);
-  TH1F * occupancy = makeTH2F("Occupancy",81,-40.5,40.5,72,0.5,72.5);
+  TH2F * occupancy_ts3 = makeTH2F("occupancy_ts3",81,-40.5,40.5,72,0.5,72.5);
+  TH2F * occupancy_ts4 = makeTH2F("occupancy_ts4",81,-40.5,40.5,72,0.5,72.5);
+  TH2F * noOfDigiHit = makeTH2F("noOfDigiHit",81,-40.5,40.5,72,0.5,72.5);
 
   for (int i = 0; i < n_events; ++i){
     
@@ -55,11 +55,15 @@ void analysisClass::loop(){
       HBHEDigi hbheDigi = hbheDigis -> GetConstituent<HBHEDigi>(iHBHEDigi);
 
       if (hbheDigi.energy() < recHitEnergyCut) continue;
-      recHitTiming -> Fill( hbheDigi.recHitTime() );
+      // recHitTiming -> Fill( hbheDigi.recHitTime() );
       recHitEnergy -> Fill( hbheDigi.energy() );
-      occupancy -> Fill( hbheDigi.ieta() , hbheDigi.iphi() , hbheDigi.fcTotal() );
+      occupancy_ts3 -> Fill( hbheDigi.ieta() , hbheDigi.iphi() , hbheDigi.fc(3) );
+      occupancy_ts4 -> Fill( hbheDigi.ieta() , hbheDigi.iphi() , hbheDigi.fc(4) );
+      noOfDigiHit -> Fill( hbheDigi.ieta() , hbheDigi.iphi() );
 
-
-    }; 
+    };
   };
+  
+  // occupancy_ts3 -> Scale( (double) 1/tuple_tree -> HBHEDigiIEta -> size() );
+  // occupancy_ts4 -> Scale( (double) 1/tuple_tree -> HBHEDigiIEta -> size() );
 }
