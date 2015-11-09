f6e54778f1e3bed44c4237b1e8f9c4a2e79c0e25
lucien1011 <lucien1011@gmail.com>
2015-11-09 11:39:20 +0100

    added draw option

--- plot_macros/plot_allHist.py
+++ plot_macros/plot_allHist.py
@@ -2,10 +2,13 @@
 import ROOT
 from PlotParser import parser
 
+parser.add_argument('-d', metavar="drawOption", dest='drawOption',action="store" , required=False, help='draw option',type=str,default="colz")
+
 ROOT.gROOT.SetBatch(ROOT.kTRUE)
 args = parser.parse_args()
 inFileName = args.inputPath
 outFileName = args.outputPath
+drawOption = args.drawOption
 
 inputFile = ROOT.TFile(inFileName,"READ")
 histNames = [k.GetName() for k in inputFile.GetListOfKeys()]
@@ -15,7 +18,7 @@ c = ROOT.TCanvas()
 c.Print(outFileName+"[")
 for i,hist in enumerate(hists):
 	hist.SetStats(0)
-	hist.Draw("p")
+	hist.Draw(drawOption)
 	c.Print(outFileName)
 c.Print(outFileName+"]")
 inputFile.Close()
