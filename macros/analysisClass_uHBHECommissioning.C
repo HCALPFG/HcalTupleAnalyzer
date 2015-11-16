7f1b5a0469ec5bb827f76320d6c65fade1f8b556
lucien1011 <lucien1011@gmail.com>
2015-11-16 17:07:25 +0100

    added TP

--- macros/analysisClass_uHBHECommissioning.C
+++ macros/analysisClass_uHBHECommissioning.C
@@ -9,6 +9,7 @@ void analysisClass::loop(){
   // Configurables
   //--------------------------------------------------------------------------------
   const int nSample = 10;
+  const double min_charge = 20.;
   
   //--------------------------------------------------------------------------------
   // Declare HCAL tree(s)
@@ -52,9 +53,9 @@ void analysisClass::loop(){
   it -> second -> fChain -> SetBranchStatus("HBHEDigiIPhi"      , kTRUE);
   it -> second -> fChain -> SetBranchStatus("HBHEDigiDepth"     , kTRUE);
   it -> second -> fChain -> SetBranchStatus("HBHEDigiSize"      , kTRUE);
-  //it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveCompressedEtSOI",kTRUE);
-  //it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveIEta",kTRUE);
-  //it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveIPhi",kTRUE);
+  it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveCompressedEtSOI",kTRUE);
+  it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveIEta",kTRUE);
+  it -> second -> fChain -> SetBranchStatus("HcalTriggerPrimitiveIPhi",kTRUE);
   };
 
   //--------------------------------------------------------------------------------
@@ -74,21 +75,40 @@ void analysisClass::loop(){
   h_fcTotal -> SetTitle(" ; uTCA Total fC ; VME Total fC ");
 
   std::map <int,TH2F*> h_fc;
+  std::map <int,TH1F*> h_uTCAfC;
+  std::map <int,TH1F*> h_vmefC;
+
   for (int ifc = 0; ifc != nSample; ifc++){
     sprintf(histName,"h_fc%d",ifc);
     h_fc[ifc] = makeTH2F(histName,50,0.,50.,50,0.,50.);
     sprintf(titleName," ; uTCA fC at TS %d ; VME fC at TS %d ",ifc,ifc);
     h_fc[ifc] -> SetTitle(titleName);
+
+    sprintf(histName,"h_uTCAfc%d",ifc);
+    h_uTCAfC[ifc] = makeTH1F(histName,50,0.,50.);
+    sprintf(titleName," ; uTCA fC at TS %d ; ",ifc);
+    h_uTCAfC[ifc] -> SetTitle(titleName);
+    
+    sprintf(histName,"h_vmefc%d",ifc);
+    h_vmefC[ifc] = makeTH1F(histName,50,0.,50.);
+    sprintf(titleName," ; vme fC at TS %d ; ",ifc);
+    h_vmefC[ifc] -> SetTitle(titleName);
   };
 
   TH2F * h_adcTotal = makeTH2F("h_adcTotal",201,-0.5,200.5,201,-0.5,200.5);
   h_adcTotal -> SetTitle(" ; uTCA Total ADC ; VME Total ADC ");
 
-  //TH1F * h_tpEt = makeTH1F("h_tpEt",50,0.,200.);
-  //h_tpEt -> SetTitle(" ; TP Et [GeV]  ; ");
+  TH2F * h_tpEt = makeTH2F("h_tpEt",50,0.,100.,50,0.,100.);
+  h_tpEt -> SetTitle(" ; uTCA TP ET ; VME TP ET");
 
-  //TH2F * h_tpOccupancy = makeTH2F("h_tpOccupancy",81,-40.5,40.5,72,0.5,72.5);
-  //h_tpEt -> SetTitle(" ; TP i#eta ; TP i#phi ");
+  //--------------------------------------------------------------------------------
+  // Custom Histograms
+  //--------------------------------------------------------------------------------
+  TH2F * h_utcaOccp_cut = makeTH2F("h_utcaOccp_cut",81,-40.5,40.5,72,0.5,72.5);
+  h_utcaOccp_cut -> SetTitle("Cut ; uTCA i#eta ; uTCA i#phi ");
+
+  TH2F * h_vmeOccp_cut = makeTH2F("h_vmeOccp_cut",81,-40.5,40.5,72,0.5,72.5);
+  h_vmeOccp_cut -> SetTitle("Cut ; VME i#eta ; VME i#phi ");
 
 
   //--------------------------------------------------------------------------------
@@ -108,15 +128,51 @@ void analysisClass::loop(){
     int n_utcaDigis = utcaDigis -> GetSize();
     int n_vmeDigis = vmeDigis -> GetSize();
 
+    //--------------------------------------------------------------------------------
+    // uTCA Digis
+    //--------------------------------------------------------------------------------
+
+    for (int i_utcaDigis = 0; i_utcaDigis < n_utcaDigis; i_utcaDigis++){
+      HBHEDigi utcaDigi = utcaDigis -> GetConstituent<HBHEDigi>(i_utcaDigis);
+      h_utcaOccp -> Fill( utcaDigi.ieta() , utcaDigi.iphi() );
+
+      if ( utcaDigi.fc(4) + utcaDigi.fc(5) > min_charge){
+	h_utcaOccp_cut -> Fill( utcaDigi.ieta() , utcaDigi.iphi() );
+      };
+
+      for (int ifc = 0; ifc != nSample; ifc++){
+        h_uTCAfC[ifc] -> Fill( utcaDigi.fc(ifc) ); 
+      };
+    };
+
+    //--------------------------------------------------------------------------------
+    // VME Digis
+    //--------------------------------------------------------------------------------
+
+    for (int i_vmeDigis = 0; i_vmeDigis < n_vmeDigis; i_vmeDigis++){
+      HBHEDigi vmeDigi = vmeDigis -> GetConstituent<HBHEDigi>(i_vmeDigis);
+      h_vmeOccp -> Fill( vmeDigi.ieta() , vmeDigi.iphi() );
+
+      if ( vmeDigi.fc(4) + vmeDigi.fc(5) > min_charge){
+	h_vmeOccp_cut -> Fill( vmeDigi.ieta() , vmeDigi.iphi() );
+      };
+
+      for (int ifc = 0; ifc != nSample; ifc++){
+        h_vmefC[ifc] -> Fill( vmeDigi.fc(ifc) ); 
+      };
+    };
+
+    //--------------------------------------------------------------------------------
+    // Matching uTCA and VME Digis
+    //--------------------------------------------------------------------------------
+
     for (int i_utcaDigis = 0; i_utcaDigis < n_utcaDigis; i_utcaDigis++){
 
       HBHEDigi utcaDigi = utcaDigis -> GetConstituent<HBHEDigi>(i_utcaDigis);
-      h_utcaOccp -> Fill( utcaDigi.ieta() , utcaDigi.iphi() );
 
       for (int i_vmeDigis = 0; i_vmeDigis < n_vmeDigis; i_vmeDigis++){
 
         HBHEDigi vmeDigi = vmeDigis -> GetConstituent<HBHEDigi>(i_vmeDigis);
-        h_vmeOccp -> Fill( vmeDigi.ieta() , vmeDigi.iphi() );
 
 	if ( ( utcaDigi.ieta() != vmeDigi.ieta() ) || ( utcaDigi.iphi() != vmeDigi.iphi() ) || ( utcaDigi.depth() != vmeDigi.depth() ) ) continue;
         
@@ -128,5 +184,29 @@ void analysisClass::loop(){
         };
       };
     };
+
+    //--------------------------------------------------------------------------------
+    // TPs
+    //--------------------------------------------------------------------------------
+
+    CollectionPtr utcaTPs (new Collection(*utca_tree, utca_tree -> HcalTriggerPrimitiveIEta -> size()));
+    CollectionPtr vmeTPs (new Collection(*vme_tree, vme_tree -> HcalTriggerPrimitiveIEta -> size()));
+
+    int n_uTCATPs = utcaTPs -> GetSize();
+    int n_vmeTPs = vmeTPs -> GetSize();
+
+    for (int i_uTCATP = 0; i_uTCATP < n_uTCATPs; i_uTCATP++){
+      HcalTP utcaTP = utcaTPs -> GetConstituent<HcalTP>(i_uTCATP);
+
+      for (int i_vmeTP = 0; i_vmeTP < n_vmeTPs; i_vmeTP++){
+        HcalTP vmeTP = vmeTPs -> GetConstituent<HcalTP>(i_vmeTP);
+        
+        if ( ( utcaTP.ieta() != vmeTP.ieta() ) || ( utcaTP.iphi() != vmeTP.iphi() )  ) continue;
+
+	h_tpEt -> Fill( utcaTP.Et() , vmeTP.Et() );
+		
+      };
+    };
+
   };
 }
