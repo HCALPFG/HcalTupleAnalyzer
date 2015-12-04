ace86f360bfefffbc4a0e51f552c650a0bf1bca2
lucien1011 <lucien1011@gmail.com>
2015-12-04 12:48:31 +0100

    added kbird in plot macro script

--- plot_macros/plot_allHist.py
+++ plot_macros/plot_allHist.py
@@ -1,6 +1,10 @@
 
 import ROOT
 from PlotParser import parser
+from Utils.rootTools import SetPalette
+
+setPalette = SetPalette()
+setPalette("kBird")
 
 parser.add_argument('-d', metavar="drawOption", dest='drawOption',action="store" , required=False, help='draw option',type=str,default="colz")
 parser.add_argument('-l', dest='logz',action="store_true" , required=False, help='draw option',default=False)
@@ -16,6 +20,8 @@ inputFile = ROOT.TFile(inFileName,"READ")
 histNames = [k.GetName() for k in inputFile.GetListOfKeys()]
 hists = [ inputFile.Get(histName) for histName in histNames]
 
+hists.sort(key=lambda x: x.GetName())
+
 c = ROOT.TCanvas()
 if setLogZ:
 	c.SetLogz()
@@ -23,6 +29,7 @@ c.Print(outFileName+"[")
 for i,hist in enumerate(hists):
 	hist.SetStats(0)
 	hist.Draw(drawOption)
+	hist.GetZaxis().SetRangeUser(0.01,1000.)
 	c.Print(outFileName)
 c.Print(outFileName+"]")
 inputFile.Close()
